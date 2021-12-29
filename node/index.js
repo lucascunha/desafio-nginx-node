647a49923f8c0e6eedacd44fcd87d77cf545d696
Lucas <lucascunha@uol.com.br>
2021-12-29 13:22:56 -0300

    upload projetos v1

---
+++ node/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Lucas')`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req,res) => {
+    res.send('<h1>Full Cycle do Lucas</h1>')
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
