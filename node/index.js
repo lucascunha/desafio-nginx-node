0da412086288699dd61b9b1693c7bc8a772b831f
Lucas <lucascunha@uol.com.br>
2021-12-31 16:03:21 -0300

    update configs

--- node/index.js
+++ node/index.js
@@ -15,7 +15,7 @@ connection.query(sql)
 connection.end()
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle do Lucas</h1>')
+    res.send('<h1>Full Cycle Rocks!</h1>')
 })
 
 app.listen(port, ()=> {
